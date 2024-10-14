b4f3f05cd2cfe95e9b2c97397c97a1b5dab0cccd
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-10-14 09:33:56 -0300

    refactor: ensure delete cascade to table projectTechnology

---
+++ prisma/migrations/20241014122945_add_cascade_delete_project_technology/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "ProjectTechnology" DROP CONSTRAINT "ProjectTechnology_projectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ProjectTechnology" DROP CONSTRAINT "ProjectTechnology_technologyId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "ProjectTechnology" ADD CONSTRAINT "ProjectTechnology_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectTechnology" ADD CONSTRAINT "ProjectTechnology_technologyId_fkey" FOREIGN KEY ("technologyId") REFERENCES "Technology"("id") ON DELETE CASCADE ON UPDATE CASCADE;
