f06fc374e8f50e51d02241c80a49227377f126bd
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-01-07 19:02:11 -0300

    feat: garantir o scroll total no mobile

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="ptBr">
       <body className={`${inter.className} antialiased`}>
-        <div className="flex h-full flex-col gap-4 overflow-hidden p-4 pb-[86px] sm:mx-auto sm:max-w-xl sm:gap-8 sm:py-8 md:max-w-2xl lg:max-w-4xl xl:max-w-[1536px] xl:flex-row xl:gap-10 xl:px-8 2xl:px-0">
+        <div className="flex flex-col gap-4 overflow-hidden p-4 pb-[86px] sm:mx-auto sm:max-w-xl sm:gap-8 sm:py-8 md:max-w-2xl lg:h-full lg:max-w-4xl xl:max-w-[1536px] xl:flex-row xl:gap-10 xl:px-8 2xl:px-0">
           <div className="xl:hidden">
             <SidebarHomeMobile />
           </div>
